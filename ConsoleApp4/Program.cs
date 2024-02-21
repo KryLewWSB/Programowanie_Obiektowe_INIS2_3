62ad82539a1db1f3d4c5a73ec09b74110ad4a334
Krystian <krystian.lewandowski@teb.edu.pl>
2024-02-21 14:33:43 +0100

    6

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,14 +1,17 @@
 ﻿using ConsoleApp4;
 
-Osoba osoba1 = new Osoba("Adam", "Nowak",500_500_500);//Deklaracja zmiennej obiektowej typu Osoba. I utworzenie w zmiennej nowego obiektu.
-Osoba osoba2 = new Osoba("Monika", "Nowak",500_500_500);
-Osoba osoba3 = new Osoba("Maria", "Kowal",500_500_500);
-Osoba osoba4 = new Osoba("Anna", "Kowalska",500_500_500);
+List<Osoba> listaOsob = new List<Osoba>();
+listaOsob.Add(new Osoba("Adam", "Nowak",500_500_500));//Deklaracja zmiennej obiektowej typu Osoba. I utworzenie w zmiennej nowego obiektu.
+listaOsob.Add( new Osoba("Monika", "Nowak",500_500_500));
+listaOsob.Add( new Osoba("Maria", "Kowal",500_500_500));
+listaOsob.Add( new Osoba("Anna", "Kowalska",500_500_500));
 
-osoba1.pobierzInformacje();
-osoba2.pobierzInformacje();
-osoba3.pobierzInformacje();
-osoba4.pobierzInformacje();
+
+foreach (var osoba in listaOsob)
+{
+    osoba.pobierzInformacje();
+}
+
 
 int liczba = 5;//Deklaracja zmiennej i typu int, oraz incjalizacja (przypisanie wartości 5)
 
