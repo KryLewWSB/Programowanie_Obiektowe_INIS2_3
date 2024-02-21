ed2a474f0650a194281e313a690b9ea7f786dcd6
Krystian <krystian.lewandowski@teb.edu.pl>
2024-02-21 13:04:48 +0100

    4

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,7 +7,8 @@ Punkt punkt2 = new Punkt(1, 2);
 Punkt punkt3 = new Punkt(1, 1);
 Punkt punkt4 = new Punkt(4, 9);
 
-Console.WriteLine($"x= {punkt1.x} y= {punkt1.y}");
-Console.WriteLine($"x= {punkt2.x} y= {punkt2.y}");
-Console.WriteLine($"x= {punkt3.x} y= {punkt3.y}");
-Console.WriteLine($"x= {punkt4.x} y= {punkt4.y}");
+
+Console.WriteLine($"x= {punkt1.getX()} y= {punkt1.y}");
+Console.WriteLine($"x= {punkt2.getX()} y= {punkt2.y}");
+Console.WriteLine($"x= {punkt3.getX()} y= {punkt3.y}");
+Console.WriteLine($"x= {punkt4.getX()} y= {punkt4.y}");
--- ConsoleApp4/Punkt.cs
+++ ConsoleApp4/Punkt.cs
@@ -2,12 +2,17 @@ namespace ConsoleApp4;
 
 public class Punkt
 {
-    public int x; 
+    private int x; 
     public int y; 
 
     public Punkt(int x, int y)
     {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
     }
 }
